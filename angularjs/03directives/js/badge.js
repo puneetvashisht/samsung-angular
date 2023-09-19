f7fc1d179ca4b87680bc5ef9bea86c55c6c3389e
PuneetMM <puneetvashisht@gmail.com>
2023-09-19 12:10:20 +0530

    directives

---
+++ angularjs/03directives/js/badge.js
@@ -0,0 +1,20 @@
+var app = angular.module('myApp', [])
+
+app.directive('sBadge', function(){
+     return{
+         templateUrl: 'partial/badge.html'
+,
+         scope: {},
+         link: function(scope, element, attrs){
+           scope.caption = attrs.caption  
+           scope.count = attrs.count  
+           console.log(attrs.caption);
+
+           element.bind('click', function(){
+             scope.count = parseInt(scope.count) + 1
+             console.log(scope.count);
+             scope.$apply();
+           })
+         }
+     }
+})
