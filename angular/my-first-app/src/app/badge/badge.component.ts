9d1347332c08a92bd1d8bebcb396815362f6dcfb
Puneet Vashisht <puneetvashisht@gmail.com>
2023-09-21 11:28:24 +0530

    creating components in angular

--- angular/my-first-app/src/app/badge/badge.component.ts
+++ angular/my-first-app/src/app/badge/badge.component.ts
@@ -7,4 +7,9 @@ import {Input, Component } from '@angular/core';
 })
 export class BadgeComponent {
     @Input('caption')caption: string = 'Default'
+    @Input('count') count : number = 0;
+
+    incrementCount(){
+      this.count = this.count + 1;
+    }
 }
